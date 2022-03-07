9e1cd9ca97891bbd4a78855d05f8b8ea2c135643
1Frank7 <86896575+1Frank7@users.noreply.github.com>
2022-03-07 15:14:00 -0600

    Metodo Delete
    Se realiza el metodo DeleteAPI para poder eliminar un json

--- App_Client/RestAPI.cs
+++ App_Client/RestAPI.cs
@@ -182,9 +182,73 @@ namespace App_Client
         }
 
 
+        /// <summary>
+        /// Metodo que sirve para realizar una consulta Delte a un RestApi
+        /// </summary>
+        /// <param name="url">La url del RestAPI a consumir</param>
+        /// <param name="data">La data que se envíara al RestAPI, puede ir vacio</param>
+        /// <param name="credenciales">Las credenciales de autorizacion="Basic " para utilizar el metodo</param>
+        /// <param name="autorization">El tipo de autorizacion="Basic " que se utilizara para consumir el RestAPI</param>
+        /// <returns>Retorna la respuesta del servidor, tal y como este la devuelve</returns>
 
 
+        public string DeleteAPI(string url, string data, string credenciales, string autorization)
+        {
+            string respuesta = "No se pudo realizar el metodo Delete";
+            //var url = $"http://localhost:8080/items/{id}";
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Headers.Add("Authorization", autorization + credenciales);
+            request.Method = "DELETE";
+            request.ContentType = "application/json";
+            request.Accept = "application/json";
+            request.Timeout = 900000;
+            using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+            {
+                streamWriter.Write(data);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+            try
+            {
+                using (WebResponse response = request.GetResponse())
+                {
+                    using (Stream strReader = response.GetResponseStream())
+                    {
+                        if (strReader == null) return respuesta;
+                        using (StreamReader objReader = new StreamReader(strReader))
+                        {
+                            string responseBody = objReader.ReadToEnd();
+                            // Do something with responseBody
+                            Console.WriteLine(responseBody);
+                            respuesta = responseBody;
+                            return respuesta;
+                        }
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+                // Handle error
+                respuesta = "Se produjo una excepcion la cual es: " + ex;
+                Console.WriteLine(respuesta);
+                //respuesta = respuesta.Replace("[{", "{");
+                //respuesta = respuesta.Replace("}]", "}");
+                return respuesta;
+            }
+        }
 
+        /// <summary>
+        /// Convierte el usuario y la contraseña en Base 64
+        /// </summary>
+        /// <param name="usuario">Usuario</param>
+        /// <param name="contraseña">Contraseña</param>
+        /// <returns></returns>
+
+        public string credenciales64(string usuario, string contraseña)
+        {
+            string credenciales = Convert.ToBase64String(ASCIIEncoding.ASCII.GetBytes(usuario + ":" + contraseña));
+            return credenciales;
+        }
 
 
     }
