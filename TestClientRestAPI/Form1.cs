c3f73650cceff258aa723ef8889114501afd135e
1Frank7 <86896575+1Frank7@users.noreply.github.com>
2022-03-07 15:33:33 -0600

    Se extraen los datos de los usuarios de la REST API
    Se crea un objeto de la clase RestAPI para usar los metodos realizados. Se realiza el consumo de la Rest API atraves de la URL y se obtiene una respuesta.

--- TestClientRestAPI/Form1.cs
+++ TestClientRestAPI/Form1.cs
@@ -14,7 +14,30 @@ namespace TestClientRestAPI
 
         private void button1_Click(object sender, EventArgs e)
         {
+            string url = "http://127.0.0.1:8000/usuarios";
+            //string usuario= "RestAPI";
+            string contraseña = "Prueba123!";
+            string autorizacion = "Basic ";
 
-        }
+
+            //Obtener usuarios
+            RestAPI consumir = new RestAPI();
+            //Creamos una lista del tipo usuarios donde tendremos los usuarios obtenidos del restapi
+            List<usuarios> usuarios = new List<usuarios>();
+            //URL del Endpoint
+            url = "http://127.0.0.1:8000/usuarios";
+            //Consumimos el Endpoint
+            string respuesta = consumir.GetAPI(url, "", "");
+
+            //Si retorna una lista vacia, es porque no existen usuarios 
+            if (respuesta.Equals("[]"))
+            {
+                //Imprimir que no encontro ningun usuario
+            }
+            //Sí son varios objetos json
+            if ((respuesta.StartsWith("[") && respuesta.EndsWith("]")))
+            {
+                usuarios = JsonConvert.DeserializeObject<List<usuarios>>(respuesta);
+            }
 
 }
